b61cb5e13baa8d090eca0adb0c39313ba4dc4057
Toni Hermoso Pulido <toniher@cau.cat>
2017-04-11 13:16:47 +0200

    test dasharray

--- src/js/Rickshaw.Graph.Renderer.DottedLine.js
+++ src/js/Rickshaw.Graph.Renderer.DottedLine.js
@@ -12,8 +12,15 @@ Rickshaw.Graph.Renderer.DottedLine = Rickshaw.Class.create( Rickshaw.Graph.Rende
 
     
     _styleSeries: function(series) {
+        var dasharray = series.dasharray ? series.dasharray : this.dasharray;
+        
         var result = Rickshaw.Graph.Renderer.Line.prototype._styleSeries.call(this, series);
-        d3.select(series.path).style("stroke-dasharray", this.dasharray);
+        d3.select(series.path).style("stroke-dasharray", dasharray);
         return result;
-    }
+    },
+    setDasharray: function(dasharray) {
+            if (dasharray !== undefined) {
+                    this.dasharray = dasharray;
+            }
+    },
 });
