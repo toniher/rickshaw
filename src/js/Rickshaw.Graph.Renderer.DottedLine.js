30f9c4217b612788a72a734a05a9bb8baf32b897
Toni Hermoso Pulido <toniher@cau.cat>
2017-04-11 12:56:46 +0200

    test custom

--- src/js/Rickshaw.Graph.Renderer.DottedLine.js
+++ src/js/Rickshaw.Graph.Renderer.DottedLine.js
@@ -2,9 +2,18 @@ Rickshaw.namespace('Rickshaw.Graph.Renderer.DottedLine');
 
 Rickshaw.Graph.Renderer.DottedLine = Rickshaw.Class.create( Rickshaw.Graph.Renderer.Line, {
     name: 'dotted_line',
+
+    defaults: function($super) {
+
+        return Rickshaw.extend( $super(), {
+            dasharray: "4, 2"
+        } );
+    },
+
+    
     _styleSeries: function(series) {
         var result = Rickshaw.Graph.Renderer.Line.prototype._styleSeries.call(this, series);
-        d3.select(series.path).style("stroke-dasharray", '5, 2');
+        d3.select(series.path).style("stroke-dasharray", this.dasharray);
         return result;
     }
 });
